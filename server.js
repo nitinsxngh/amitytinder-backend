8e0ebd6b3fb7e39c8f415d580648d4a5560f365d
Nitin Singh <nitinsxngh@gmail.com>
2024-12-08 14:33:03 +0530

    Change in app dot use serve static file

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ mongoose
 
 // Serve Static Files (React Frontend)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use('/react-tinder-card-demo', express.static(path.join(__dirname, 'frontend/build')));
+
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'));
